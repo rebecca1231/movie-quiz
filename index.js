c2fa513192930810a8e5bf516179c3f6297877ed
Rebecca <rebeccagetsanemail@gmail.com>
2020-10-27 17:14:04 +0900

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { ApolloServer, PubSub } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server-express");
 const mongoose = require("mongoose");
 const { RESTDataSource } = require('apollo-datasource-rest');
 require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
 const  MONGO_URI  = process.env.MONGO_URI;
 const typeDefs = require("./GraphQl/typeDefs");
@@ -42,6 +45,7 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubsub }),
 });
 
+app.use(cors())
 //setup db
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -49,9 +53,25 @@ mongoose
     console.log(`Connected to ${db.connections[0].name}`);
   })
 
+  if (process.env.NODE_ENV === "production") {
+    //express will serve up production assets
+    //like main.js file, or main.css file
+    app.use(express.static("client/build"));
+  
+    //express will serve up index.html file when
+    //it doesn't recognize the route
+    const path = require("path");
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
+  server.applyMiddleware({
+    path: '/client', // you should change this to whatever you want
+    app,
+  });
+  
 
-
-server.listen(PORT, () =>
+app.listen(PORT, () =>
     console.log(
         `Server started, listening on port ${PORT} for incoming requests.`,
     ),
